ad3c334effbcdc548b50b0ae7373658ba941525f
ninghao <1123300681@qq.com>
2019-01-10 17:09:45 +0800

    少1到5屏的开机动画

--- js/index.js
+++ js/index.js
@@ -6,9 +6,21 @@ window.onload=function () {
     var  conten=document.querySelector('.contentMain')
     var  contlist=document.querySelector('.contentMain li')
     var loo=document.querySelector('.loo')
+    var teamlists=document.querySelector('.team_lists')
+    var teamli=document.querySelectorAll('.team_lists li')
+    var can = document.getElementById('mycanvas');
+    var aa =document.querySelectorAll('.nav li')
+    var up=document.querySelector('.mengban .up')
+    var down=document.querySelector('.mengban .down')
+    var baitiao=document.querySelector('.mengban .baitiao')
+    var pp=0;
+    var arr = ['bg1.jpg','bg2.jpg','bg3.jpg','bg4.jpg','bg5.jpg','about1.jpg','about2.jpg','about3.jpg','about4.jpg','worksimg1.jpg','worksimg2.jpg','worksimg3.jpg','worksimg4.jpg','team.png','greenLine.png'];
+    var kk=can.getContext('2d');
     var num=0
     var oo=0;
-    // console.log(conten)
+    var gn=true
+    var gn1=false;
+    console.log(listnodes)
     whit.style.left=listnodes[0].offsetLeft+listnodes[0].offsetWidth/2-whit.offsetWidth/2+'px'
     //绑定及点击小白事件
     for (var i = 0; i<listnodes.length;i++ ){
@@ -17,8 +29,10 @@ window.onload=function () {
             whit.style.left=this.offsetLeft+this.offsetWidth/2-whit.offsetWidth/2+'px'
             for (var j =0; j<listnodes.length;j++){
                 listup[j].style.width=''
+                aa[j].className=''
             }
             listup[this.mun].style.width='100%'
+            aa[this.mun].className='aa'
             conten.style.top=-this.mun*contlist.offsetHeight+'px'
              num=this.mun
         }
@@ -46,6 +60,10 @@ window.onload=function () {
                 conten.style.top=-num*contlist.offsetHeight+'px'
                 move(num)
             }
+           for (var j = 0; j <aa .length; j++) {
+               aa[j].className=''
+           }
+           aa[num].className='aa'
         },500)
 
     }
@@ -128,7 +146,131 @@ window.onload=function () {
             listup[num].style.width='100%'
         }
     }
+    function canvas() {
+        var arrp = [];
+        pp()
+        function pp() {
+            setTimeout(function () {
+                var x = Math.random() * can.width;
+                var y = can.height;
+                var cr = Math.random() * 10 + 2;
+                var deg = 0;
+                var s = Math.random() * 50 + 20;
+                var r = Math.floor(Math.random() * 255);
+                var g = Math.floor(Math.random() * 255);
+                var b = Math.floor(Math.random() * 255);
+                arrp.push({
+                    x: x,
+                    y: y,
+                    cr: cr,
+                    deg: deg,
+                    s: s,
+                    r: r,
+                    g: g,
+                    b: b
+                });
+                if(gn1)return
+                pp();
+            }, 50);
+        }
+        fn()
+        function fn() {
+            setTimeout(function () {
+                kk.clearRect(0, 0, can.width, can.height);
+                for (var i = 0; i < arrp.length; i++) {
+                    arrp[i].deg += 5;
+                    kk.fillStyle = 'rgba(' + arrp[i].r + ',' + arrp[i].g + ',' + arrp[i].b + ')';
+                    var deg = arrp[i].deg * Math.PI / 180;
+                    var x = Math.sin(deg) * arrp[i].s + arrp[i].x;
+                    var y = arrp[i].y - deg * arrp[i].s;
+                    kk.beginPath();
+                    kk.arc(x, y, arrp[i].cr, 0, 2 * Math.PI, false);
+                    kk.fill()
+                }
+                if(gn1)return
+                fn()
+            }, 1000 / 60)
+        }
+
+    }
+
+    //第5屏
+
+teamlists.onmouseleave=function () {
+    kk.clearRect(0,0,can.width,can.height);
+    can.style.display='none'
+    gn1=true;
+    gn=true
+    for (var j = 0; j <teamli .length; j++) {
+        teamli[j].style.opacity='1'
+    }
+}
+for (var i = 0; i < teamli.length; i++) {
+        teamli[i].k=i
+        teamli[i].onmouseenter=function () {
+            for (var j = 0; j <teamli .length; j++) {
+                teamli[j].style.opacity='0.5'
+            }
+            kk.clearRect(0,0,can.width,can.height);
+            can.style.left=this.k*teamli[0].offsetWidth+'px'
+            console.log(2)
+            can.style.display='block'
+            gn1=false;
+            this.style.opacity='1'
+            if (gn) {
+                canvas()
+                gn=false
+            }
+        }
+    }
+
+     //侧边导航
+    for (var i = 0; i <aa .length; i++) {
+        aa[i].yy=i;
+        aa[i].onclick=function () {
+            num=this.yy;
+            for (var j = 0; j <aa .length; j++) {
+                aa[j].className=''
+            }
+            this.className='aa'
+            conten.style.top=-num*contlist.offsetHeight+'px'
+            move(num)
+        }
+    }
+
+    //开场
+
+    for (var i = 0; i <arr .length; i++) {
+        var img=new Image()
+        img.src='img/'+arr[i]+''
+        img.onload=function () {
+            pp++
+            console.log(pp/arr.length*100)
+            baitiao.style.width=pp/arr.length*100+'%'
+            if(pp==arr.length){
+                up.style.height=0
+                down.style.height=0
+                baitiao.style.display='none'
+            }
+            if(document.addEventListener){
+                up.addEventListener('transitionend',fn1)
+                function fn1() {
+                  up.parentElement.remove();
+                    up.removeEventListener('transitionend',fn1)
+                }
+            }
+        }
+    }
 
 }
+
+
+
+
+
+
+
+
+
 
 
